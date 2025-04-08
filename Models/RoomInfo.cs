aacbd5327c0a8274b0f411d036538d810be1f7d9
Akbar Shazada <shahzadaakbar12@gmail.com>
2025-04-08 19:41:06 +0100

    Chnage

---
+++ Models/RoomInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace IndoorNavigation.Models
+{
+    public class RoomInfo
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Floor { get; set; } = string.Empty;
+    }
+}
---
+++ Services/RoomSearchService.cs
@@ -0,0 +1,22 @@
+﻿using IndoorNavigation.Models;
+
+namespace IndoorNavigation.Services
+{
+    public class RoomSearchService
+    {
+        public List<RoomInfo> Rooms { get; set; } = new();
+
+        public RoomSearchService()
+        {
+            // Load all rooms from static RoomData
+            Rooms = RoomData.Rooms;
+        }
+
+        public RoomInfo? FindRoom(string query)
+        {
+            return Rooms.FirstOrDefault(r =>
+                r.Name.Equals(query, StringComparison.OrdinalIgnoreCase) ||
+                r.Id.Equals(query, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
