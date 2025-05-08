773beadd2374be9afaa1a6cbd0487d153f59bd85
Akbar Shazada <shahzadaakbar12@gmail.com>
2025-05-08 11:44:39 +0100

    DemoInter

---
+++ Models/RoomInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace INTERACTIVEMAP.Models
+{
+    public class RoomInfo
+    {
+        public string Id { get; set; }
+        public string Floor { get; set; }
+        public string Name { get; set; }
+    }
+}
+
