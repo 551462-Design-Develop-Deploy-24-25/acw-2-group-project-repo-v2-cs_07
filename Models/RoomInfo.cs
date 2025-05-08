fe255614587b14cec380c447fe855152b093eca7
Akbar Shazada <shahzadaakbar12@gmail.com>
2025-05-08 11:06:53 +0100

    Delete Models directory

--- Models/RoomInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IndoorNavigation.Models
-{
-    public class RoomInfo
-    {
-        public string Id { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string Floor { get; set; } = string.Empty;
-    }
-}
